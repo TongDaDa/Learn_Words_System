3096a282a052eca3362f57e49eb3ec5f04da8e4c
liutong <console90@163.com>
2018-04-24 22:35:27 +0800

    1

--- src/config.js
+++ src/config.js
@@ -3,5 +3,3 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
     REQUEST_URL = 'http://127.0.0.1:8899'
 }
 const UPDATE_FILE_URL = `${REQUEST_URL}/file/uploadfile`;
-
-module.exports = {UPDATE_FILE_URL,REQUEST_URL};
